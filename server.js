a26acd17ef6561983ab9c100badb0147574d432f
Karina Titov <karinasul1995@gmail.com>
2020-06-22 13:09:39 -0500

    added controllers

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 const express = require("express");
-const path = require("path");
 
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
+const PORT = process.env.PORT || 3001;
 
-
-// Define middleware here
+// Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Add routes, both API and view
+app.use(routes);
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleSearch");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googleSearch",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
-// Use apiRoutes
-app.use("/api", apiRoutes);
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-  
-  app.listen(PORT, function() {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
-  });
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
